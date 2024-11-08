24946454592b8871a1128e7b6e9017364e45ae6b
Tanguygab <tanguy@gabril.com>
2024-11-08 13:55:32 +0100

    Reformat Code

--- main.c
+++ main.c
@@ -3,45 +3,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int main(int argc, char * argv[]){
- if(SDL_Init(SDL_INIT_EVERYTHING) !=0){
- fprintf(stderr,"ErrorinSDL_Init:%s\n",SDL_GetError());
- exit(EXIT_FAILURE);
- }
- SDL_Surface *ecran=NULL;
- if((ecran=SDL_SetVideoMode(800,600,32,SDL_HWSURFACE|
- SDL_DOUBLEBUF))==NULL){
- fprintf(stderr,"ErrorinSDL_SetVideoMode:%s\n",
- SDL_GetError());
- SDL_Quit();
- exit(EXIT_FAILURE);
- }
- SDL_WM_SetCaption("Sonic The Hedgehog",NULL);
- /*Votretravailici*/
- SDL_Rect rectangle;
- rectangle.x = 50;
- rectangle.y = 50;
- rectangle.w = 200;
- rectangle.h = 100;
- SDL_FillRect(ecran, &rectangle, SDL_MapRGB(ecran->format, 64, 118, 173));
- SDL_Flip(ecran);
- SDL_Rect rectangle2;
- rectangle2.x = 200;
- rectangle2.y = 50;
- rectangle2.w = 100;
- rectangle2.h = 200;
- SDL_FillRect(ecran, &rectangle2, SDL_MapRGB(ecran->format, 64, 140, 110));
- SDL_Flip(ecran);
- /*Votretravailici*/
- int active=1;
- SDL_Event event;
- while(active){
- SDL_WaitEvent(&event);
- switch(event.type){
- case SDL_QUIT:active=0; break;
- default :break;
- }
- }
- SDL_Quit();
- exit(EXIT_SUCCESS);
- }
+void addRect(SDL_Surface * screen, int x, int y, int w, int h, int r, int g, int b);
+
+int main(int argc, char * argv[]){
+    if(SDL_Init(SDL_INIT_EVERYTHING) !=0){
+        fprintf(stderr,"ErrorinSDL_Init:%s\n",SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Surface * screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    if (screen == NULL) {
+        fprintf(stderr,"ErrorinSDL_SetVideoMode:%s\n",
+        SDL_GetError());
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_WM_SetCaption("Sonic The Hedgehog",NULL);
+
+    addRect(screen, 50, 50, 200, 100, 64, 118, 173);
+    addRect(screen, 200, 50, 100, 200, 64, 140, 110);
+
+    int active = 1;
+    SDL_Event event;
+    while(active){
+        SDL_WaitEvent(&event);
+        switch(event.type){
+            case SDL_QUIT:
+                active=0;
+                break;
+            case SDL_KEYUP:
+                printf("key pressed!");
+                break;
+            default: break;
+        }
+    }
+    SDL_Quit();
+    exit(EXIT_SUCCESS);
+}
+void addRect(SDL_Surface * screen, int x, int y, int w, int h, int r, int g, int b) {
+    SDL_Rect rectangle;
+    rectangle.x = x;
+    rectangle.y = y;
+    rectangle.w = w;
+    rectangle.h = h;
+    SDL_FillRect(screen, &rectangle, SDL_MapRGB(screen->format, r, g, b));
+    SDL_Flip(screen);
+}
