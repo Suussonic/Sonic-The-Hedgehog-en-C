af5eb78e016f327437fc68b5e1e7ec2951535cca
Tanguygab <tanguy@gabril.com>
2024-12-06 07:46:18 +0100

    Remove background color from images that have one

--- main.c
+++ main.c
@@ -1,20 +1,12 @@
 #define SDL_MAIN_HANDLED
 
 #include <SDL.h>
-#include <SDL_rotozoom.h>
 #include "sprites.c"
 
 int WINDOW_WIDTH = 800;
 int WINDOW_HEIGHT = 255;
 
 SDL_Surface * screen;
-
-typedef struct {
-    SDL_Surface * image;
-    SDL_Rect sprite;
-    SDL_Rect pos;
-    int flipped;
-} Sprite;
 
 Sprite sonic;
 
@@ -34,7 +26,7 @@ int flippedX(Sprite sprite, int x) {
 
 void flipSprite(Sprite * sprite) {
     sprite->flipped = !sprite->flipped;
-    sprite->image = zoomSurface(sprite->image, -1, 1, 1);
+    sprite->image = zoomSurf(sprite, -1, 1, 1);
     sprite->sprite.x = sprite->image->w - sprite->sprite.x - sprite->sprite.w;
 }
 
@@ -64,15 +56,27 @@ int main(int argc, char * argv[]) {
     //addRect(screen, 50, 50, 200, 100, getRGB(64, 118, 173));
     //addRect(screen, 200, 50, 100, 200, getRGB(64, 140, 110));
 
-    loadImages();
+    loadImages(screen);
 
     //SDL_Surface * titleScreenBg = images[TITLE_SCREEN];
     //SDL_Rect titleScreenBgSprite = getPos(24, 213, 1024, 112);
     //SDL_Rect titleScreenBgPos = getPos(0, 0, -1, -1);
 
-    sonic.image = images[SONIC];
-    sonic.sprite = getPos(43, 257, 32, 40);
-    sonic.pos = getPos(25, 140, -1, -1);
+    SDL_Surface * background = images[GREEN_HILL_BACKGROUND];
+    SDL_Rect backgroundSprite0 = getPos(24, 181, 1024, 32);
+    SDL_Rect backgroundPos0 = getPos(0, 0, -1, -1);
+    SDL_Rect backgroundSprite1 = getPos(24, 221, 1024, 16);
+    SDL_Rect backgroundPos1 = getPos(0, 32, -1, -1);
+    SDL_Rect backgroundSprite2 = getPos(24, 245, 1024, 16);
+    SDL_Rect backgroundPos2 = getPos(0, 48, -1, -1);
+    SDL_Rect backgroundSprite3 = getPos(24, 269, 1024, 48);
+    SDL_Rect backgroundPos3 = getPos(0, 64, -1, -1);
+    SDL_Rect backgroundSprite4 = getPos(24, 325, 1024, 40);
+    SDL_Rect backgroundPos4 = getPos(0, 112, -1, -1);
+    SDL_Rect backgroundSprite5 = getPos(24, 373, 1024, 104);
+    SDL_Rect backgroundPos5 = getPos(0, 152, -1, -1);
+
+    sonic = setSprite(SONIC, getPos(43, 257, 32, 40), getPos(25, 140, -1, -1));
     int isSneaking = 0;
 
     SDL_Surface * GreenHillChunks = images[GREEN_HILL_CHUNKS];
@@ -166,6 +170,12 @@ int main(int argc, char * argv[]) {
         }
 
         //showImage(titleScreenBg, titleScreenBgSprite, titleScreenBgPos);
+        showImage(background, backgroundSprite0, backgroundPos0);
+        showImage(background, backgroundSprite1, backgroundPos1);
+        showImage(background, backgroundSprite2, backgroundPos2);
+        showImage(background, backgroundSprite3, backgroundPos3);
+        showImage(background, backgroundSprite4, backgroundPos4);
+        showImage(background, backgroundSprite5, backgroundPos5);
         showImage(GreenHillChunks, GreenHillSprite, GreenHillPos);
         showImage(sonic.image, sonic.sprite, sonic.pos);
         SDL_Flip(screen);
