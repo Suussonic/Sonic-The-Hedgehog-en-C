867fcb377d4c54f5491b22d884d9a41c585d6b86
Suussonic <150890672+Suussonic@users.noreply.github.com>
2025-02-05 17:31:00 +0100

    code title.c

--- src/title.c
+++ src/title.c
@@ -1,21 +1,68 @@
+#include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("=== Bienvenue ===\n");
-    printf("Appuyez sur Entrée pour lancer le programme principal...\n");
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
-    // Attendre que l'utilisateur appuie sur Entrée
-    getchar();
-
-    // Lancer le programme principal
-    int status = system("./main");
-    
-    // Vérifier si l'exécution de main s'est bien passée
-    if (status == -1) {
-        perror("Erreur lors de l'exécution de main");
-        return 1;
+int main(int argc, char *argv[]) {
+    // Initialisation de SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Erreur SDL_Init : %s\n", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    // Création de la fenêtre
+    SDL_Window *window = SDL_CreateWindow("Title Screen", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s\n", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    // Création du rendu
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Erreur SDL_CreateRenderer : %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    // Couleurs
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Fond noir
+    SDL_RenderClear(renderer);
+
+    // Création du bouton Play
+    SDL_Rect playButton = {WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 30, 200, 60}; // Position et taille du bouton
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Couleur verte
+    SDL_RenderFillRect(renderer, &playButton);
+    SDL_RenderPresent(renderer);
+
+    int running = 1;
+    SDL_Event event;
+    
+    // Boucle d'événements
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = 0;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.x >= playButton.x && event.button.x <= playButton.x + playButton.w &&
+                        event.button.y >= playButton.y && event.button.y <= playButton.y + playButton.h) {
+                        system("./game"); // Exécute le fichier du jeu (à compiler séparément)
+                        running = 0;
+                    }
+                    break;
+            }
+        }
+    }
+
+    // Nettoyage SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
